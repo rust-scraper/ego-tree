8d85a811aef6b03d8361d340ff54d47d6103414a
Curtis McEnroe <programble@gmail.com>
2016-01-04 08:16:20 -0500

    Test ancestors iterator

---
+++ tests/iter.rs
@@ -0,0 +1,19 @@
+extern crate id_tree;
+
+use id_tree::{Tree, Ref};
+
+#[test]
+fn ancestors() {
+    let mut tree = Tree::new('a');
+    let b = tree.root_mut().append('b');
+    let c = tree.get_mut(b).append('c');
+    let d = tree.get_mut(c).append('d');
+
+    let d = tree.get(d);
+    let mut iter = d.ancestors();
+    assert_eq!(Some(&'d'), iter.next().as_ref().map(Ref::value));
+    assert_eq!(Some(&'c'), iter.next().as_ref().map(Ref::value));
+    assert_eq!(Some(&'b'), iter.next().as_ref().map(Ref::value));
+    assert_eq!(Some(&'a'), iter.next().as_ref().map(Ref::value));
+    assert_eq!(None, iter.next());
+}
